b3f3e6ef1b409e5da5519d9b4b78b5fcecb4f566
David Mauricio <117051183+DavidMauricioDev@users.noreply.github.com>
2025-02-11 11:49:25 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+function calculate(){
+    var produto =(document.getElementById("PRODUTO").value)
+    var ipi=document.getElementById("ipi").value
+    var frete=document.getElementById("frete").value
+    var icmsNF=document.getElementById("icmsNF").value
+    var aliqInt=document.getElementById("aliqInt").value
+    
+    var icmsCalculado = ((produto + ipi + frete - icmsNF) / 0.795)*aliqInt;
+    console.log("Valor do icms fronteira é:" + icmsCalculado)
+
+
+   
+
+}
