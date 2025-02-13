e2f1312c38f588bb1daf08e757b6ac5a60a15976
Jorge Otávio <jorgelosb@gmail.com>
2025-02-12 23:43:45 -0300

    feat: using new ids to access values

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function calculate() {
-  var produto = document.getElementById("PRODUTO").value;
+  var produto = document.getElementById("product_value").value;
   var ipi = document.getElementById("ipi").value;
-  var frete = document.getElementById("frete").value;
-  var icmsNF = document.getElementById("icmsNF").value;
-  var aliqInt = document.getElementById("aliqInt").value;
+  var frete = document.getElementById("freight_value").value;
+  var icmsNF = document.getElementById("icms_nf").value;
+  var aliqInt = document.getElementById("aliquota_interestadual").value;
 
   var icmsCalculado = ((produto + ipi + frete - icmsNF) / 0.795) * aliqInt;
   console.log("Valor do icms fronteira é:" + icmsCalculado);
