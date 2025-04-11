b3875c2a5bdbf61302ccdb8a7975186e77c4ccc4
David Mauricio <117051183+DavidMauricioDev@users.noreply.github.com>
2025-04-10 21:23:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ function calculate() {
   let frete = document.getElementById("freight_value").value;
   let icmsNF = document.getElementById("icms_nf").value;
   let aliqInt = document.getElementById("aliquota_interestadual").value;
+  let iva = document.getElementById("indice_valor_agregado").value;
   
-  let icmsCalculado = ((produto + ipi + frete - icmsNF) / 0.795) * aliqInt;
+  let icmsCalculado = ((produto + ipi + frete - icmsNF) / 0.795) * iva;
   
   document.querySelector('.result_box').style.display = 'block'
   document.querySelector('.result_box_value').innerHTML = icmsCalculado
